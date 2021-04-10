3727798fe07a07ef963ec02cff1923d404174e53
Lê Rôn <>
2021-04-10 04:54:22 +0000

    add function

--- Code/collaborativeFiltering.c
+++ Code/collaborativeFiltering.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+void inputMatrix(char fileName[],float matrix[50][50]);
+void xuat_matran(float matrix[50][50],int numberOfUsers,int numberOfItems);
+float gia_tri_trung_binh(float matrix[50][50],int user,int numberOfItems);
+float similarity(float matrix[50][50],float avgMatrix[50][50],int user1,int user2,int numberOfItems);
+void outputMatrix(char fileName[],float matrixOut[50][50]);
 int main(){
   printf("Hello World");
   return 0;
