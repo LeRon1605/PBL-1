5be8b3d5e0037ebc1219721f22ac2c6d72a47ab5
Ron Le <ronle9519@gmail.com>
2021-04-11 10:07:15 +0700

    Edit file

--- Work/Quang/xuatMatran.c
+++ Work/Quang/xuatMatran.c
@@ -1,8 +1,6 @@
-#include<stdio.h>
-Void OutputMatrix(char fileName[], float matrix[50][50], int numberOfUsers, int numberOfItems ){
-	for(int i = 0; i <= numberOfUsers, i++) {
-		for ( int j = 0; j <= numberOfItems; j++) 
-		printf("%f\t",&matrix[i][j]);
+void xuat_matran(float matrix[50][50],int numberOfUsers,int numberOfItems){
+	for(int i = 1; i <= numberOfUsers, i++) {
+		for ( int j = 0; j <= numberOfItems; j++) printf("%f\t",&matrix[i][j]);
 		printf("\n");
 	}	
 }	
