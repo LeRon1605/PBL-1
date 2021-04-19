45bc1f7dbee2a4d96b8968bb7db96820c9197a51
Lê Rôn <>
2021-04-19 13:55:12 +0000

    add

--- Code/collaborativeFiltering.c
+++ Code/collaborativeFiltering.c
@@ -5,13 +5,17 @@ void inputMatrix(char fileName[], float matrix[50][50], int &numberOfUsers, int 
 void outputMatrix(char fileName[], float matrixOut[50][50], int columns,int rows); // Hàm xuất ma trận ra file
 float getAvgRatingOfUser(float matrix[50][50],int user,int numberOfItems); // Hàm lấy giá trị rating trung bình của người dùng
 float getSimilarity(float matrix[50][50],float avgMatrix[50][50],int user1,int user2,int numberOfItems); // Hàm lấy giá trị sim giữa hai người dùng 
-
+void getNeighbor(float simMatrix[50][50],int numberOfUsers,int user, int k, int arr[]); // Hàm lấy neighbor
+float getRating(float matrix[50][50], float simMatrix[50][50],float avgMatrix[50][50], int numberOfUsers, int numberOfItems, int k, int arr[], int user, int item);
+void swap(float *a, float *b);
 int main(){
   int numberOfUsers, numberOfItems; 
   float matrix[50][50];
   float avgMatrix[50][50];
   float simMatrix[50][50];
   inputMatrix("input.inp", matrix, numberOfUsers, numberOfItems);
+  int k = numberOfUsers/2;
+  int arr[k + 1];
   printf("Input matrix\n");
   xuat_matran(matrix, numberOfUsers, numberOfItems);
   outputMatrix("output.out", matrix, numberOfUsers, numberOfItems);
@@ -32,11 +36,32 @@ int main(){
     for (int j = 1;j <= numberOfUsers;j++){
       simMatrix[i][j] = getSimilarity(matrix, avgMatrix, i, j, numberOfItems);
     }
-  } 
+  }  
   printf("\n\n\n");
   printf("Sim matrix\n");
-  xuat_matran(simMatrix, numberOfUsers, numberOfUsers);
-
+  xuat_matran(simMatrix, numberOfUsers, numberOfUsers); 
+  /*
+  for (int i = 1;i <= numberOfUsers;i++){
+    getNeighbor(simMatrix, numberOfUsers, i, k, arr);
+    for (int j = 1;j <= numberOfItems;j++){
+      if (matrix[i][j] == 0) matrix[i][j] = getRating(matrix, simMatrix, avgMatrix, numberOfUsers, numberOfItems, k, arr, i, j);
+    }
+  }
+  /*
+  printf("%f",getRating(matrix, simMatrix,avgMatrix, numberOfUsers, numberOfItems, k, arr, 3, 5));
+   for (int i = 1;i <= k;i++) printf(" %d ",arr[i]);
+  printf("\n\n\n");
+  */
+  for (int i = 1;i <= numberOfUsers;i++){
+    printf("%d: ",i);
+    getNeighbor(simMatrix, numberOfUsers, i, k, arr);
+    for (int j = 1;j <= numberOfUsers;j++){
+      printf("%d ",arr[j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n\n");
+  xuat_matran(matrix, numberOfUsers, numberOfItems); 
   return 0;
 }
 
@@ -111,4 +136,34 @@ float getSimilarity(float matrix[50][50],float avgMatrix[50][50],int user1,int u
     }
   }
   return (tuSo/(sqrt(sumOfUser1)*sqrt(sumOfUser2)));
+}
+void swap(float *a, float *b){
+  float temp = *a;
+  *a = *b;
+  *b = temp;
+}
+void getNeighbor(float simMatrix[50][50],int numberOfUsers,int user, int k, int arr[]) {
+  for (int i = 1;i <= numberOfUsers;i++) arr[i] = i;
+  int n =numberOfUsers;
+  for (int i = 1;i <= k;i++){
+    for (int j = 1;j < n;j++){
+      if (simMatrix[user][j] > simMatrix[user][j + 1] && simMatrix[user][j] != 1 && simMatrix[user][j + 1] != 1){
+        swap(&simMatrix[user][j],&simMatrix[user][j + 1]);
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+    n--;
+  }
+}
+float getRating(float matrix[50][50], float simMatrix[50][50],float avgMatrix[50][50], int numberOfUsers, int numberOfItems, int k, int arr[], int user, int item){
+  float avgRating = getAvgRatingOfUser(matrix,user,numberOfItems);
+  float tuSo = 0;
+  float mauSo = 0;
+  for (int i = 1;i <= k;i++){
+     tuSo += getSimilarity(matrix,avgMatrix,user,arr[i],numberOfItems)*avgMatrix[arr[i]][item];
+     mauSo += abs(getSimilarity(matrix,avgMatrix,user,arr[i],numberOfItems));
+  }
+  return avgRating + (tuSo/mauSo);
 }
